77f891a0f445200da6cf47ebd376cbc8a196f26a
NasirSayed <nasihere@gmail.com>
2021-10-08 00:49:55 -0400

    Access cafe

--- src/app/customer/customer.component.ts
+++ src/app/customer/customer.component.ts
@@ -3,4 +3,8 @@ import { Component } from '@angular/core';
 @Component({ templateUrl: 'customer.component.html' })
 export class CustomerComponent {
     step:number = 1;
+    picUploader(id: string) {
+        document.getElementById(id).click();
+
+    }
 }
