5a9e8588bddcf92ca23d22ffa66254cd3b6708ba
NasirSayed <nasihere@gmail.com>
2021-10-07 23:59:21 -0400

    fresh page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-﻿import { NgModule } from '@angular/core';
+﻿import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
@@ -22,6 +22,8 @@ import { MaterialModule } from './material/material.module'
 import { CustomerLookupComponent } from './customer-lookup/customer-lookup.component';
 
 @NgModule({
+    
+  schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
     imports: [
         BrowserModule,
         ReactiveFormsModule,
