e5734cc61a86b005b71a66e5e6b1aebbde1d883a
NasirSayed <nasihere@gmail.com>
2021-10-24 14:38:32 -0400

    imporantt

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 
 Angular 10 - 
 # myCafeUi
+
+SSL CERTIFICATE
+https://awswithatiq.com/letsencrypt-with-amazon-linux-2-centos-7/
+https://www.youtube.com/watch?v=adQDNRZ59r0
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,11 +5,11 @@
 export const environment = {
   production: false,
   
-  apiUrl: 'http://ec2-3-132-213-115.us-east-2.compute.amazonaws.com:5000',
-  filUploadHost: 'http://ec2-3-132-213-115.us-east-2.compute.amazonaws.com:3000'
+  // apiUrl: '/api',
+  // filUploadHost: '/upload'
   
-  // apiUrl: 'http://localhost:5000',
-  //filUploadHost: 'http://localhost:3000'
+  apiUrl: 'http://localhost:5000',
+  filUploadHost: 'http://localhost:3000'
 };
 
 /*
