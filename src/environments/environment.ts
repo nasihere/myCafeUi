2a7cd8518b307bd5228a53c40f25e5152d53f1a9
Nasz <nasihere@gmail.com>
2021-10-24 01:40:40 -0400

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,10 +6,10 @@ export const environment = {
   production: false,
   
   apiUrl: 'http://ec2-3-132-213-115.us-east-2.compute.amazonaws.com:5000',
-  // filUploadHost: 'http://ec2-3-132-213-115.us-east-2.compute.amazonaws.com:3000'
+  filUploadHost: 'http://ec2-3-132-213-115.us-east-2.compute.amazonaws.com:3000'
   
   // apiUrl: 'http://localhost:5000',
-  filUploadHost: 'http://localhost:3000'
+  //filUploadHost: 'http://localhost:3000'
 };
 
 /*
