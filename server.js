18aee9ddf2e7db2471622fd3ae4333266dda46bd
NasirSayed <nasihere@gmail.com>
2021-10-24 13:07:02 -0400

    imporantt

--- server.js
+++ server.js
@@ -1,16 +1,29 @@
-const express = require('express');
-const path = require('path')
-const port = 8080;
-const app = express();
+const https = require('https');
+const fs = require('fs');
 
-app.use(express.static('./dist/mycafe-ui'))
-app.set('view engine', 'pug');
+const options = {
+  key: fs.readFileSync('key.pem'),
+  cert: fs.readFileSync('cert.pem'),
+  ca: fs.readFileSync('ca.pem')
+};
 
-app.get('/*', (req, res) => {
-    res.sendFile('/dist/mycafe-ui/index.html',{root:__dirname})
-});
+https.createServer(options, function (req, res) {
+  res.writeHead(200);
+  res.end("hello world\n");
+}).listen(8080);
+// const express = require('express');
+// const path = require('path')
+// const port = 8080;
+// const app = express();
+
+// app.use(express.static('./dist/mycafe-ui'))
+// app.set('view engine', 'pug');
+
+// app.get('/*', (req, res) => {
+//     res.sendFile('/dist/mycafe-ui/index.html',{root:__dirname})
+// });
 
 
-app.listen(port, () => {
-    console.log("Server is listening on port "+port);
-});
+// app.listen(port, () => {
+//     console.log("Server is listening on port "+port);
+// });
