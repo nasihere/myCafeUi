fd5f068db8a6f5da4298147087aea990d78d7903
NasirSayed <nasihere@gmail.com>
2021-10-24 01:47:29 -0400

    imporantt

--- dist/mycafe-ui/main.js
+++ dist/mycafe-ui/main.js
@@ -8807,9 +8807,9 @@ __webpack_require__.r(__webpack_exports__);
 const environment = {
     production: false,
     apiUrl: 'http://ec2-3-132-213-115.us-east-2.compute.amazonaws.com:5000',
-    // filUploadHost: 'http://ec2-3-132-213-115.us-east-2.compute.amazonaws.com:3000'
+    filUploadHost: 'http://ec2-3-132-213-115.us-east-2.compute.amazonaws.com:3000'
     // apiUrl: 'http://localhost:5000',
-    filUploadHost: 'http://localhost:3000'
+    //filUploadHost: 'http://localhost:3000'
 };
 /*
  * For easier debugging in development mode, you can import the following file
